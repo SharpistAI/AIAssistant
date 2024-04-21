f468948adf2fcf5138e23c50ac55fa6896a0d7c9
azizxon <azizxon_uz@hotmail.com>
2024-04-21 11:15:51 +0500

    add files, and nested

---
+++ Model/NavItem.cs
@@ -0,0 +1,10 @@
+﻿namespace AIAssistant.Model
+{
+    public class NavItem
+    {
+        public string Text { get; set; }
+        public string Href { get; set; }
+        public bool HasSubmenu { get; set; }
+        public List<NavItem> SubItems { get; set; } = new List<NavItem>();
+    }
+}
