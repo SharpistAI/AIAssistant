07df20fe8e024dae1ca1f6d958764bbbf9637c5a
azizxon <azizxon_uz@hotmail.com>
2024-04-21 19:44:13 +0500

    fix issue

--- AIClient.cs
+++ AIClient.cs
@@ -7,7 +7,7 @@ namespace AIAssistant
         public async Task<string> GetResponseAsync(string documentName,string keyword, string questionType)
         {
             var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Post, $"https://localhost:7261/api/assistant/Getquestions?documentName={documentName}&keyword={keyword}&questionType={questionType}");
+            var request = new HttpRequestMessage(HttpMethod.Post, $"http://172.178.40.71:8080/api/assistant/Getquestions?documentName={documentName}&keyword={keyword}&questionType={questionType}");
             var response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
@@ -16,7 +16,7 @@ namespace AIAssistant
         public async Task<string> EvaluateAnswerAsync(string documentName,string keyword,string question, string userAnswer)
         {
             var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Post, $"https://localhost:7261/api/assistant/Getquestions?documentName={documentName}&keyword={keyword}$question={question}&userAnswer={userAnswer}");
+            var request = new HttpRequestMessage(HttpMethod.Post, $"http://172.178.40.71:8080/api/assistant/Getquestions?documentName={documentName}&keyword={keyword}$question={question}&userAnswer={userAnswer}");
             var response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
